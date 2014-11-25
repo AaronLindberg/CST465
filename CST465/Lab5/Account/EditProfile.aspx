765d292b36ca416d7dc4cc87da2eabfce1024d9d
AaronLindberg <aaron.lindberg@oit.edu>
2014-11-24 20:59:01 -0800

    Added Edit Profile
    Edit Profile Uses Database

---
+++ CST465/Lab5/Account/EditProfile.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeBehind="EditProfile.aspx.cs" Inherits="Lab5.Account.EditProfile" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <title>Edit Profile</title>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="heading" runat="server">
+    <h1>Edit Profile</h1>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="body" runat="server">
+    <div>
+        <fieldset>
+            <legend>User Info</legend>
+            <asp:Label AssociatedControlID="uxFirstName" Text="First Name" runat="server"></asp:Label>
+            <asp:TextBox ID="uxFirstName" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label AssociatedControlID="uxLastName" Text="Last Name" runat="server"></asp:Label>
+            <asp:TextBox ID="uxLastName" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="uxSubmit" Text="Submit Changes" OnClick="uxSubmit_Click" runat="server" />
+        </fieldset>
+    </div>
+</asp:Content>
---
+++ CST465/Lab5/Account/EditProfile.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace Lab5.Account
+{
+    public partial class EditProfile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlSecurityDB"].ConnectionString);
+            SqlCommand command = new SqlCommand("SELECT UserId, FirstName, LastName FROM UserProfile Where UserId = @UserId;", connection);
+            try
+            {
+                command.Parameters.AddWithValue("UserId", Membership.GetUser().ProviderUserKey);
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows && reader.Read())
+                {
+                    uxFirstName.Text = (string)reader.GetValue(1);
+                    uxLastName.Text = (string)reader.GetValue(2);
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        protected void uxSubmit_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlSecurityDB"].ConnectionString);
+            SqlCommand command = new SqlCommand("UserProfile_InsertUpdate", connection);
+            try
+            {
+                command.Parameters.AddWithValue("UserId", Membership.GetUser().ProviderUserKey);
+                command.Parameters.AddWithValue("FirstName", uxFirstName.Text);
+                command.Parameters.AddWithValue("LastName", uxLastName.Text);
+
+                command.CommandType = CommandType.StoredProcedure;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            finally
+            {
+                connection.Close();
+            }
+            
+        }
+    }
+}
